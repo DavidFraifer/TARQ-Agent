f026482ef2232ee53d7156b8d6c2b68883a971fa
DavidFraifer <130702843+DavidFraifer@users.noreply.github.com>
2025-08-27 19:27:56 +0200

    Update websearch.py

--- tarq_agent/tools/websearch.py
+++ tarq_agent/tools/websearch.py
@@ -13,3 +13,7 @@ class WebSearch:
         
         console.tool(f"[WEBSEARCH]", task_id=task_id)
 
+        # Esto es lo que se ejecuta cuando llegue al modulo de search
+        # Si quieres imprimir algo usa console.tool
+        # Si quieres guardar algo en memoria usa self.task_memory.set()
+        # La request del usuario la tienes en user_input
